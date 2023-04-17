c9375f5f265bd08f1dc4069e28cea5210868ceec
Lizge BOZKURT <bozkurtlizge@gmail.com>
2023-04-17 09:50:07 +0300

    Add project files.

---
+++ csvFile/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace csvFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dosya_oku = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.ekle = new System.Windows.Forms.Button();
+            this.sil = new System.Windows.Forms.Button();
+            this.güncelle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dosya_oku
+            // 
+            this.dosya_oku.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.dosya_oku.Location = new System.Drawing.Point(512, 45);
+            this.dosya_oku.Name = "dosya_oku";
+            this.dosya_oku.Size = new System.Drawing.Size(107, 23);
+            this.dosya_oku.TabIndex = 1;
+            this.dosya_oku.Text = "DOSYA OKU ";
+            this.dosya_oku.UseVisualStyleBackColor = false;
+            this.dosya_oku.Click += new System.EventHandler(this.dosya_oku_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 158);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NAME:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(173, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "POPULATİON:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LENGTH:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(173, 322);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(173, 357);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(188, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // ekle
+            // 
+            this.ekle.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ekle.Location = new System.Drawing.Point(418, 280);
+            this.ekle.Name = "ekle";
+            this.ekle.Size = new System.Drawing.Size(118, 35);
+            this.ekle.TabIndex = 10;
+            this.ekle.Text = "EKLE";
+            this.ekle.UseVisualStyleBackColor = false;
+            this.ekle.Click += new System.EventHandler(this.ekle_Click);
+            // 
+            // sil
+            // 
+            this.sil.BackColor = System.Drawing.Color.Red;
+            this.sil.Location = new System.Drawing.Point(418, 322);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(118, 35);
+            this.sil.TabIndex = 11;
+            this.sil.Text = "SİL";
+            this.sil.UseVisualStyleBackColor = false;
+            this.sil.Click += new System.EventHandler(this.sil_Click);
+            // 
+            // güncelle
+            // 
+            this.güncelle.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.güncelle.Location = new System.Drawing.Point(418, 363);
+            this.güncelle.Name = "güncelle";
+            this.güncelle.Size = new System.Drawing.Size(118, 29);
+            this.güncelle.TabIndex = 12;
+            this.güncelle.Text = "GÜNCELLE";
+            this.güncelle.UseVisualStyleBackColor = false;
+            this.güncelle.Click += new System.EventHandler(this.güncelle_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(582, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.güncelle);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.ekle);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dosya_oku);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button dosya_oku;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button ekle;
+        private System.Windows.Forms.Button sil;
+        private System.Windows.Forms.Button güncelle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ csvFile/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.DataGridTextBoxColumn;
+namespace csvFile
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+
+            //dataGridView a 3 sütun ekliyoruz.
+           
+
+
+        }
+
+
+
+            private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getData(string filePath)
+        {
+            DataTable dataTable = new DataTable();
+            string[] satir = System.IO.File.ReadAllLines(filePath);
+            if (satir.Length > 0)
+            {
+                //başlık satırı
+                string firstTitle = satir[0];
+                string[] title = firstTitle.Split(',');
+                foreach (string basliklar in title)
+                {
+                    dataTable.Columns.Add(new DataColumn(basliklar));
+                }
+                //Veriler için kodlarımız
+                for (int i = 1; i < satir.Length; i++)
+                {
+                    string[] veriler = satir[i].Split(',');
+                    DataRow dataRow = dataTable.NewRow();
+                    int columnIndex = 0;
+                    foreach (string data in title)
+                    {
+                        dataRow[data] = veriler[columnIndex++];
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+            if (dataTable.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = dataTable;
+            }
+
+        }
+
+        private void dosya_oku_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+            getData(textBox1.Text);
+
+        }
+
+        private void güncelle_Click(object sender, EventArgs e)
+        {
+
+      //oluşan griedviewi csv dosyasına ekleyip yeni dosyayı tekrar gridviewe yazdır
+           
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+            getData(textBox1.Text);
+
+        }
+
+        private void ekle_Click(object sender, EventArgs e)
+        {
+            
+            int row= 0; 
+            dataGridView1.Rows[row].Selected = true;
+
+           
+            int column = 0; 
+            dataGridView1.Rows[row].Cells[column].Value = textBox2.Text;
+            column++;
+            dataGridView1.Rows[row].Cells[column].Value = textBox3.Text;
+            column  ++;
+            dataGridView1.Rows[row].Cells[column].Value = textBox4.Text;
+
+
+
+            MessageBox.Show("DEĞİŞİKLİĞİ KAYDETMEK İÇİN OK TUŞUNA BASINIZ...");
+
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            if
+(dataGridView1.SelectedRows.Count > 0)
+
+            {
+
+                //seçili satırı siler.
+
+
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+                MessageBox.Show("DEĞİŞİKLİĞİ KAYDETMEK İÇİN OK TUŞUNA BASINIZ...");
+            }
+            else
+            {
+
+                MessageBox.Show("SİLİNECEK SATIRI SEÇMEDİNİZ!");
+
+            }
+
+
+
+
+        }
+
+    
+
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter StreamWriter = new StreamWriter(@"C:\Users\EXCALIBUR\Videos\mammals.csv"))
+            {
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    StreamWriter.Write(dataGridView1.Columns[i].HeaderText);
+                    if (i != dataGridView1.Columns.Count - 1)
+                    {
+                        StreamWriter.Write(",");
+                    }
+                }
+                StreamWriter.Write(StreamWriter.NewLine);
+
+                
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                    {
+                        StreamWriter.Write(row.Cells[i].Value);
+                        if (i != dataGridView1.Columns.Count - 1)
+                        {
+                            StreamWriter.Write(",");
+                        }
+                    }
+                    StreamWriter.Write(StreamWriter.NewLine);
+                }
+            }
+            MessageBox.Show("DOSYA GÜNCELLENDİ...");
+
+        }
+    }
+    }
+
+
